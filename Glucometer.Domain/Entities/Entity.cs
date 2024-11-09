596516c5e1e5e0f4b393b506559711276ac59f5c
Kauã Vinícius <kauaponte@hotmail.com>
2024-11-09 09:54:10 -0300

    Domain

---
+++ Glucometer.Domain/Entities/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace Glucometer.Domain.Entities;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
---
+++ Glucometer.Domain/Entities/GlucoseTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Glucometer.Domain.Entities;
+
+public class GlucoseTest : Entity
+{
+    public GlucoseTest() { }
+    public GlucoseTest(ushort value)
+    {
+        Value = value;
+    }
+
+    public GlucoseTest(ushort value, DateTime time)
+    {
+        Value = value;
+        Time = time;
+    }
+
+    public ushort Value { get; set; } = 100;
+    public DateTime Time { get; set; } = DateTime.UtcNow;
+}
